e1a83087bb02b8a258c8c402f15bb302f6054127
@skruru <s14k_ex-fw-myt@ezweb.ne.jp>
2020-07-06 16:43:59 +0900

    全選択完成

--- js/all_select.js
+++ js/all_select.js
@@ -1,5 +1,46 @@
 // DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
 document.addEventListener('DOMContentLoaded', function () {
   // 全選択をクリックしたときのイベントをセットします
+  const selectAll = document.getElementById('select-all');
+  const items = Array.from(document.getElementsByClassName('js-check'));
+  const itemNames = document.getElementById('selected-items');
+
+  const addItems = checkedItems => {
+    itemNames.innerHTML = '';
+    checkedItems.forEach(checkedItem => {
+      const cloneItem = checkedItem.parentNode.querySelector('span').cloneNode(true);
+      itemNames.appendChild(cloneItem);
+    });
+  };
+
+  selectAll.addEventListener('click', e => {
+    items.forEach(item => item.checked = e.target.checked);
+    const checkedItems = items.filter(item => item.checked === true);
+    addItems(checkedItems);
+  },false);
   // 各アイテムをクリックしたときのイベントをセットします
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      const checkedItems = items.filter(item => item.checked === true);
+      addItems(checkedItems);
+      
+      // 全アイテム数とチェックされているアイテム数が同じ
+      if (items.length === checkedItems.length) {
+        selectAll.checked = true;
+        selectAll.indeterminate = false;
+        return;
+      }
+
+      // チェックされているアイテムが無い
+      if (checkedItems.length === 0) {
+        selectAll.checked = false;
+        selectAll.indeterminate = false;
+        return;
+      }
+
+      // 一部がチェックされている
+      selectAll.checked = false;
+      selectAll.indeterminate = true;
+    });
+  });
 }, false);
